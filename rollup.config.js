c845954584d39abbbe9d16077fd8baf753d68f57
meh <meh@schizofreni.co>
2020-10-18 21:04:12 +0200

    chore: initial commit

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: 'dist/index.js', format: 'cjs' },
+    { file: 'dist/index.mjs', format: 'es' }
+  ],
+  external: ['path', 'fs-extra'],
+  plugins: []
+};
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import { rollup } from 'rollup';
+import loadConfigFile from 'rollup/dist/loadConfigFile';
+
+export default function barrel(options = {}) {
+	return {
+		name: 'rollup-plugin-rollup',
+
+		async load(id) {
+			if (!id.endsWith('.rollup')) {
+				return null;
+			}
+
+			const config = path.resolve(__dirname, id);
+			const { options, warnings } = await loadConfigFile(config, { format: 'es' });
+			warnings.flush();
+
+			if (options.length != 1) {
+				console.error(`${id} must produce a single output`);
+				return null;
+			}
+
+			const bundle = await rollup(options[0]);
+			const generated = await bundle.generate({ format: 'es' });
+			const output = generated.output[0];
+
+			return {
+				code: `export default ${JSON.stringify(output.code)}`,
+				map: { mappings: '' }
+			};
+		}
+	}
+}
